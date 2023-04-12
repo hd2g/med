d1f111a52bfa3bd32986d3583081429af1446bc7
hd2g <nagisa.159s.8673.trap@gmail.com>
2023-04-12 22:29:25 +0900

    feat: add view about layout template for the time being...

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,15 +1,19 @@
 module Main exposing (..)
+import Html exposing (Html)
 
-import Update exposing (update)
-import View exposing (view)
+-- import Update exposing (update)
+-- import View exposing (view)
 
-import Browser
+-- import Browser
 
-main = Browser.sandbox
-  { init = 1
-  , update = update
-  , view = view
-  }
+-- main = Browser.sandbox
+--   { init = 1
+--   , update = update
+--   , view = view
+--   }
+
+main =
+  Html.text "ok"
 
 undefined : () -> a
 undefined _ = Debug.todo "undefined!"
--- frontend/src/View.elm
+++ frontend/src/View.elm
@@ -2,17 +2,28 @@ module View exposing (..)
 
 import Update exposing (..)
 
-import Html exposing (Html, div, button, text)
+import Html exposing (Attribute, Html, div, button, text)
 import Html.Events exposing (onClick)
 import Main exposing (undefined)
+import Model exposing (Model)
+
+type alias HtmlElement = List (Attribute msg) -> List (Html msg) -> Html msg
+
+layout : HtmlElement
+layout = undefined ()
 
 emptyLine : () -> Html msg
-emptyLine = undefined
+emptyLine = undefined ()
 
--- view : Model -> Html Msg
+title : HtmlElement
+title = undefined ()
+
+page : HtmlElement
+page = undefined ()
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+  layout []
+    [ title [] []
+    , page [] []
     ]
